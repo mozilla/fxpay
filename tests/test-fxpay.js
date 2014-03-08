72af81dc869f0a468aecaec59716cca2fd6d8e14
Kumar McMillan <kumar.mcmillan@gmail.com>
2014-03-07 19:38:07 -0600

    poll transaction until ready (bug 981228)

--- tests/test-fxpay.js
+++ tests/test-fxpay.js
@@ -19,13 +19,6 @@ describe('fxpay', function () {
       var productId = 123;
       var webpayJWT = '<base64 JWT>';
 
-      server.respondWith(
-        'POST',
-        /http.*\/payments\/in\-app\/purchase\/product\/123/,
-        [200, {"Content-Type": "application/json"},
-         JSON.stringify({webpayJWT: webpayJWT,
-                         contribStatusURL: '/somewhere'})]);
-
       fxpay.purchase(productId, {
         onpurchase: function(err) {
           if (!err) {
@@ -37,14 +30,33 @@ describe('fxpay', function () {
         mozPay: mozPay
       });
 
+      // Respond to fetching the JWT.
+      server.respondWith(
+        'POST',
+        /http.*\/payments\/in\-app\/purchase\/product\/123/,
+        [200, {"Content-Type": "application/json"},
+         JSON.stringify({webpayJWT: webpayJWT,
+                         contribStatusURL: '/transaction/XYZ'})]);
       server.respond();
+
       mozPay.returnValues[0].onsuccess();  // resolve the DOM request.
+
+      // Respond to polling the transaction.
+      // TODO: make the state value realistic.
+      server.respondWith(
+        'POST',
+        /http.*\/transaction\/XYZ/,
+        [200, {"Content-Type": "application/json"},
+         JSON.stringify({state: 'COMPLETED'})]);
+      server.respond();
     });
 
     it('should report XHR abort', function (done) {
       server.respondWith(function(xhr, id) {
         // This is dumb but xhr.abort() triggers load first.
         xhr.dispatchEvent(new sinon.Event("abort", false, false, xhr));
+        // Prevent future listening.
+        xhr.eventListeners = {};
       });
 
       fxpay.purchase(123, {
@@ -61,6 +73,8 @@ describe('fxpay', function () {
     it('should report XHR errors', function (done) {
       server.respondWith(function(xhr, id) {
         xhr.dispatchEvent(new sinon.Event("error", false, false, xhr));
+        // Prevent future listening.
+        xhr.eventListeners = {};
       });
 
       fxpay.purchase(123, {
@@ -108,6 +122,73 @@ describe('fxpay', function () {
         mozPay: mozPay
       });
 
+      server.respond();
+    });
+
+    it('should timeout polling the transaction', function (done) {
+      var productId = 123;
+
+      fxpay.purchase(productId, {
+        onpurchase: function(err) {
+          console.log('GOT error', err);
+          assert.equal(err, 'TRANSACTION_TIMEOUT');
+          done();
+        },
+        mozPay: mozPay,
+        maxTries: 2,
+        pollIntervalMs: 1
+      });
+
+      // Respond to fetching the JWT.
+      server.respondWith(
+        'POST',
+        /http.*\/payments\/in\-app\/purchase\/product\/123/,
+        [200, {"Content-Type": "application/json"},
+         JSON.stringify({webpayJWT: '<jwt>',
+                         contribStatusURL: '/transaction/XYZ'})]);
+      server.respond();
+
+      mozPay.returnValues[0].onsuccess();  // resolve the DOM request.
+
+      // Respond to polling the transaction.
+      // TODO: make the state value realistic.
+      server.autoRespond = true;
+      server.respondWith(
+        'POST',
+        /http.*\/transaction\/XYZ/,
+        [200, {"Content-Type": "application/json"},
+         JSON.stringify({state: 'PENDING'})]);
+      server.respond();
+    });
+
+    it('should report invalid transaction state', function (done) {
+      var productId = 123;
+
+      fxpay.purchase(productId, {
+        onpurchase: function(err) {
+          assert.equal(err, 'INVALID_TRANSACTION_STATE');
+          done();
+        },
+        mozPay: mozPay
+      });
+
+      // Respond to fetching the JWT.
+      server.respondWith(
+        'POST',
+        /http.*\/payments\/in\-app\/purchase\/product\/123/,
+        [200, {"Content-Type": "application/json"},
+         JSON.stringify({webpayJWT: '<jwt>',
+                         contribStatusURL: '/transaction/XYZ'})]);
+      server.respond();
+
+      mozPay.returnValues[0].onsuccess();  // resolve the DOM request.
+
+      // Respond to polling the transaction.
+      server.respondWith(
+        'POST',
+        /http.*\/transaction\/XYZ/,
+        [200, {"Content-Type": "application/json"},
+         JSON.stringify({state: 'THIS_IS_NOT_A_VALID_STATE'})]);
       server.respond();
     });
   });

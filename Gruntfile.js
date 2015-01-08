c29a9882b273d0d016ebd390b41c050c8b63880a
Kumar McMillan <kumar.mcmillan@gmail.com>
2015-01-08 16:30:41 -0600

    Stop pretending to use require (bug 1113801)
    The old require() stub was very node-like which
    means it is not compatible with RequireJS and this
    caused problems when trying to plug fxpay into
    Fireplace.
    
    This patch isn't ideal but maybe we can fix it later
    by doing proper module-fi-cation. All the patch does
    is stop making namespace checks look like calls to
    require() since they're not.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ var packager = require('./tasks/packager');
 
 // List module files in order so that calls to require() work right:
 var libFiles = [
-  __dirname + '/lib/require.js',
+  __dirname + '/lib/fxpay/init_module.js',
   __dirname + '/lib/fxpay/utils.js',
   __dirname + '/lib/fxpay/settings.js',
   __dirname + '/lib/fxpay/api.js',
